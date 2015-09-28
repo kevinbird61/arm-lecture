ed9b550319e643304536974e68b444c5f491467f
kevinbird61 <kevinbird61@gmail.com>
2015-09-28 11:39:21 +0800

    Replace the orginal pseudo code to assembly code:
    Use tail-recursion to implement recursion version
    and Test flag using

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -11,42 +11,26 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
-
-	@ R4 = R0 - 0 (update flags)
-	sub r4,r0,#0
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-	ble .L3
-	@ Compare R4 wtih 1
-	cmp r4,#1
-	@ If R4 == 1 goto .L4 (which returns 1)
-	beq .L4
+	push {r4, r5, lr}
 	
-	@ R0 = R4 - 1
-	sub r0,r4,#1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-	blx fibonacci
+	subs r4,r0,#0		@ update flags
+	beq .Loop
 	
-	@ R5 = R0
-	mov r0,r5
-	@ R0 = R4 - 2
-	sub r0,r4,#2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-	blx fibonacci
-
-	@ R0 = R5 + R0 (update flags)
-	add r0,r5,r0
+	add r5,r1,#0		@ r5 = seq1
 	
-	pop {r3, r4, r5, pc}		@EPILOG
+	sub r0,r4,#1		@ r0 = r4 - 1
+	add r1,r5,r2		@ r1 = r5 + r2
+	mov r2,r5		@ r2 = r5
+	bl fibonacci
+	
+	pop {r4, r5, pc}		@EPILOG
 	
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
 
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+.Loop:
+	sub r0,r2,#0
+	pop {r4,r5,pc}
+	
 	
 	.size fibonacci, .-fibonacci
 	.end
